2e154454476f9a036cc35b2524f66c8f9c7a6f72
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2025-12-02 03:50:28 -0800

    fix: return a display cars page according to role

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Car;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CarController extends Controller
 {
@@ -11,6 +12,8 @@ class CarController extends Controller
 
         $cars = Car::with('owner','media')->paginate(10);
 
+        if(Auth::user()->profile->role == "owner")
+            return view('owner.cars.index');
         return view('cars.index',[
             'cars' => $cars,
         ]);
