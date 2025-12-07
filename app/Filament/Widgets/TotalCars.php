3ba9792ba20e8bfb7a6a92a69abffea67ccc41a9
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2025-12-07 12:22:22 -0800

    fix: not showing fixed

--- app/Filament/Widgets/TotalCars.php
+++ app/Filament/Widgets/TotalCars.php
@@ -15,17 +15,19 @@ class TotalCars extends StatsOverviewWidget
 
     protected function getStats(): array
     {
+        // dd(now()->month,Transaction::whereMonth('created_at', now()->month)->get());
         return [
              Stat::make('Total Cars', Car::count())
             ->icon('heroicon-o-truck')
             ->color('primary'),
 
-            Stat::make('Active Rentals', Booking::where('availability_status', 'booked')->count())
+            Stat::make('Active Rentals', Booking::where('status', 'confirmed')->count())
                 ->icon('heroicon-o-clock')
                 ->color('warning'),
 
-            Stat::make('Revenue This Month', '$' . Transaction::whereMonth('created_at', now())->sum('amount'))
+            Stat::make('Revenue This Month', 'ETB ' . number_format(Transaction::whereMonth('created_at', now()->month)->sum('amount'),2))
                 ->icon('heroicon-o-currency-dollar')
+                // ->chart()
                 ->color('success'),
         ];
     }
