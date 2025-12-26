1d5617003d5f6cb625c92e8f9b92558b4738a786
abdulkerim <114300499+AbdulkerimA@users.noreply.github.com>
2025-12-25 17:36:28 -0800

    del: filament admin panel completly removed

--- app/Http/Controllers/DashBoard/CarController.php
+++ app/Http/Controllers/DashBoard/CarController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
-use function Filament\authorize;
+// use function Filament\authorize;
 
 class CarController extends Controller
 {
